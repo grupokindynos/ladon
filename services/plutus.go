6980d6e672bfe05bd59191a5f6d68870ccea43a6
helios <helios@polispay.org>
2020-05-29 13:20:22 -0500

    Update plutus.go

--- services/plutus.go
+++ services/plutus.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -75,6 +76,7 @@ func (p *PlutusRequests) SubmitPayment(body plutus.SendAddressBodyReq) (txid str
 	var tokenString string
 	err = json.Unmarshal(tokenResponse, &tokenString)
 	if err != nil {
+		log.Println("error unmarshalling plutus response", tokenResponse)
 		return txid, err
 	}
 	headerSignature := res.Header.Get("service")
